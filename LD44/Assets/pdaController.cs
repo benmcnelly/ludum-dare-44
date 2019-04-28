3f41b85c8c6ae93598168ef3057375f48364a7ab
Ben <ben@fbstudios.com>
2019-04-28 06:58:48 -0500

    Lighting

---
+++ LD44/Assets/pdaController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pdaController : MonoBehaviour
+{
+    public Text self;
+    public static string pda_update;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        self.text = pda_update;
+    }
+}
---
+++ LD44/Assets/pda_previewImage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pda_previewImage : MonoBehaviour
+{
+    Sprite spriteImageCompenent;
+
+    public Sprite loaded_sprite;
+    public static Sprite current_sprite;
+
+    private void Awake()
+    {
+        spriteImageCompenent = GetComponent<Sprite>();
+    }
+
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (current_sprite)
+        {
+            spriteImageCompenent = current_sprite;
+        }
+    }
+
+    void updateSprite()
+    {
+
+    }
+
+}
