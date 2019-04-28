52898d09dcfe71335e0b768083693a21c3f5caae
Ben <ben@fbstudios.com>
2019-04-28 02:11:31 -0500

    CODEZ

---
+++ LD44/Assets/BagController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BagController : MonoBehaviour
+{
+
+    // Public variables we want to expose to the editor...
+    public int bag_weight = 0;
+    public int monies = 0;
+    public int health = 40;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
